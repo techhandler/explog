8472e2c3221fc5be51a6cc2abb237f6a18f0cff5
sumil <sumilprincejain@gmail.com>
2020-03-17 15:03:20 +0530

    adding db

--- db/index.js
+++ db/index.js
@@ -14,5 +14,45 @@ export const initiateDb = () => {
     if (!res || !res.length)
       AsyncStorage.setItem(dataBase.ledger, JSON.stringify([]))
   })
+}
 
+export default class db {
+  constructor(t) {
+    this.tableName = t
+    return this
+  }
+
+  static table(t) {
+    return new db(t)
+  }
+
+  async insert(obj) {
+    let oldItems = await AsyncStorage.getItem(this.tableName)
+    oldItems = JSON.parse(oldItems)
+    await AsyncStorage.setItem(this.tableName, JSON.stringify([obj, ...oldItems]))
+    return [obj]
+  }
+
+  async find(index) {
+    let items = await AsyncStorage.getItem(this.tableName)
+    if (typeof index === 'number')
+      return JSON.parse(items)[index]
+    return JSON.parse(items)
+  }
+
+  delete(index) {
+    this._delete = index
+    return this
+  }
+
+  update(index, obj) {
+    this._updateIndex = index
+    this._updateValue = obj
+    return this
+  }
+
+  json() {
+    console.log(this)
+    return this
+  }
 }
