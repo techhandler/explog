8ebdf6248462c801c72db26e19c6c4d4136b474d
sumil <sumilprincejain@gmail.com>
2020-03-20 10:26:37 +0530

    re arranging

---
+++ Constants.js
@@ -0,0 +1,41 @@
+export { Color, currentScreen, style, dataBase }
+
+const Color = {
+  violet: '#957bfb',
+  indigo: '#4477fa',
+  blue: '#2a9dfb',
+  cyan: '#57f9fa',
+  aqua: '#68ea8e',
+  green: '#6de427',
+  yellow: '#fce939',
+  orange: '#f98b30',
+  red: '#f43e2b',
+  pink: '#ed83fd',
+  magenta: '#e3538c',
+  white: '#ffffff',
+  gray: '#dddddd',
+  silver: '#888888',
+  black: '#000000'
+}
+
+const dataBase = {
+  ledger: 'ledger',
+  category: 'category',
+  account: 'account'
+}
+
+const currentScreen = {
+  insertLedger: 'insertLedger'
+}
+
+const style = {
+  paper: {
+    flex: 1,
+    padding: 20
+  },
+  inputText: {
+    borderBottomWidth: 1,
+    padding: 5,
+    fontSize: 20
+  }
+}
---
+++ components/LedgerDetail.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react'
+import { Text, View, TextInput, FlatList, Picker, Alert, ToastAndroid } from "react-native"
+import FabButton from './FabButton'
+import { Color, style } from '../Constants'
+import { insertLedger, fetchAllAccounts, fetchAllCategories } from '../db'
+
+const initialState = {
+  ledgerName: "",
+  ledgerAmount: '',
+  selectedCategory: '',
+  notes: '',
+  selectedDate: '',
+  accountId: ''
+}
+
+class LedgerDetail extends Component {
+  state = {
+    ...initialState,
+    category: [],
+    accounts: [],
+    showCalender: false
+  }
+
+  async componentDidMount() {
+    const {result : accounts} = await fetchAllAccounts()
+    let accountDefault = accounts.filter(a=>a.defaultAccount);
+    accountDefault = accountDefault && accountDefault[0] ? accountDefault[0] : accounts[0];
+    const {result : category} = await fetchAllCategories()
+    this.setState({accounts, category, accountId:accountDefault.accountId})
+  }
+
+  handleOnSave = async () => {
+    let isError = false
+    if (!validate(this.state, 'ledgerName')) {
+      ToastAndroid.show('Name is mandatory', ToastAndroid.SHORT)
+      isError = true
+    }
+    if (!validate(this.state, 'ledgerAmount')) {
+      ToastAndroid.show('Amount is mandatory', ToastAndroid.LONG)
+      isError = true
+    }
+    if (!validate(this.state, 'selectedCategory')) {
+      ToastAndroid.show('Category is mandatory', ToastAndroid.LONG)
+      isError = true
+    }
+    if (!isError) {
+      const res = await insertLedger(this.state)
+      console.log('resrser', res)
+      if (res.success) {
+        this.setState({...initialState})
+      }
+    }
+  }
+
+
+  render() {
+    return (
+      <View style={style.paper}>
+
+        <TextInput
+          style={style.inputText}
+          onChangeText={ledgerName => this.setState({ledgerName})}
+          placeholder={'Expense'}
+          value={this.state.ledgerName}
+        />
+        <View style={{height: 20}}/>
+        <TextInput
+          style={style.inputText}
+          onChangeText={ledgerAmount => this.setState({ledgerAmount})}
+          placeholder={'0.00'}
+          keyboardType={'decimal-pad'}
+          value={this.state.ledgerAmount + ''}
+        />
+        <View style={{height: 20}}/>
+        <Picker
+          selectedValue={this.state.selectedCategory}
+          style={{height: 50}}
+          onValueChange={selectedCategory => this.setState({selectedCategory})}>
+          {
+            this.state.category.map(a => <Picker.Item label={a.categoryName} value={a.categoryId} key={a.categoryId}/>)
+          }
+        </Picker>
+
+        <TextInput
+          value={this.state.notes + ''}
+          style={{borderWidth: 1}}
+          multiline
+          numberOfLines={16}
+          onChangeText={notes => this.setState({notes})}
+          editable
+          maxLength={200}
+        />
+
+        <Picker
+          selectedValue={this.state.accountId}
+          style={{height: 50}}
+          onValueChange={accountId => this.setState({accountId})}>
+          {
+            this.state.accounts.map(a => <Picker.Item label={a.accountName} value={a.accountId} key={a.accountId}/>)
+          }
+        </Picker>
+
+        <View>
+          <Text>Date</Text>
+        </View>
+
+        <FabButton
+          text="*"
+          onPress={() => this.handleOnSave()}
+          // style={{backgroundColor: Color.blue}}
+        />
+      </View>
+    )
+  }
+}
+
+export default LedgerDetail
+
+
+const validate = (obj, key) => {
+  if (obj.hasOwnProperty(key)) {
+    const value = obj[key]
+    if (typeof value === 'string' && !value.length) {
+      return false
+    } else if (typeof value === 'number' && !(value > 0))
+      return false
+    return true
+  }
+  return false
+}
