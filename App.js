3ac9b06df7a066f883a3a58e0cafdab81c2ffba1
sumil <sumilprincejain@gmail.com>
2020-03-26 14:22:22 +0530

    adding footer

--- App.js
+++ App.js
@@ -49,68 +49,71 @@ export default class App extends Component {
 
   render() {
     return (
-      <ViewPagerAndroid style={styles.viewPager} initialPage={0}>
-        <View key="1">
-          <Text style={styles.header}>CHATS</Text>
-          <View style={{flex: 1}}>
-            {this.state.currentScreen === currentScreen.home &&
-            <Ledger setGlobalState={this.setGlobalState} state={this.state}/>}
+      <>
+        <ViewPagerAndroid style={styles.viewPager} initialPage={0}>
+          <View key="1">
+            <Text style={styles.header}>CHATS</Text>
+            <View style={{flex: 1}}>
+              {this.state.currentScreen === currentScreen.home &&
+              <Ledger setGlobalState={this.setGlobalState} state={this.state}/>}
 
-            {this.state.currentScreen === currentScreen.insertLedger && <LedgerDetail insert={true}/>}
+              {this.state.currentScreen === currentScreen.insertLedger && <LedgerDetail insert={true}/>}
 
-            {this.state.currentScreen === currentScreen.detailLedger &&
-            <LedgerDetail childData={this.state.childData}/>}
+              {this.state.currentScreen === currentScreen.detailLedger &&
+              <LedgerDetail childData={this.state.childData}/>}
+            </View>
           </View>
-        </View>
-        <View key="2">
-          <Text style={styles.header}>ACC</Text>
-          {this.state.currentScreen === currentScreen.home &&
-          <Accounts setGlobalState={this.setGlobalState} state={this.state}/>}
+          <View key="2">
+            <Text style={styles.header}>ACC</Text>
+            {this.state.currentScreen === currentScreen.home &&
+            <Accounts setGlobalState={this.setGlobalState} state={this.state}/>}
 
-          {this.state.currentScreen === currentScreen.insertAccount && <AccountDetail/>}
+            {this.state.currentScreen === currentScreen.insertAccount && <AccountDetail/>}
 
-          {this.state.currentScreen === currentScreen.detailAccount &&
-          <AccountDetail childData={this.state.childData}/>}
-        </View>
-        <View key="3">
-          <Text>Dev Page</Text>
-          <Button
-            title="Remove All"
-            onPress={async () => {
-              await AsyncStorage.clear()
-              console.log('removed all')
-            }}/>
-          <View style={{height: 20}}/>
-          <Button
-            title="Seed All Data"
-            onPress={async () => {
-              await AsyncStorage.setItem('ledger', JSON.stringify([
-                {lI: "1", lN: "L-4", lA: 400, lC: "1", lNo: "Testing 4", lD: 1584884861245, aI: "3"},
-                {lI: "2", lN: "L-3", lA: 540, lC: "2", lNo: "Testing 3", lD: 1584884703808, aI: "1"},
-                {lI: "3", lN: "L-2", lA: 200, lC: "3", lNo: "Testing 2", lD: 1584884528782, aI: "3"},
-                {lI: "4", lN: "L-1", lA: 100, lC: "1", lNo: "Testing 1", lD: 1584884455243, aI: "2"}
-              ]))
-              await AsyncStorage.setItem('account', JSON.stringify([
-                {aI: '1', aN: 'A-1', aA: 20000, d: false, lg: [{add: 5000}]},
-                {aI: '2', aN: 'A-2', aA: 40000, d: true, lg: [{add: 10000}]},
-                {aI: '3', aN: 'A-3', aA: 60000, d: false, lg: [{add: 30000}]}
-              ]))
-              await AsyncStorage.setItem('category', JSON.stringify([
-                {cI: '1', cN: 'Other'},
-                {cI: '2', cN: 'C-2'},
-                {cI: '3', cN: 'C-3'}
-              ]))
-              console.log("seeding done")
-            }}/>
-          <View style={{height: 20}}/>
-          <Button
-            title="Show Data"
-            onPress={async () => {
-              console.log('ledger>>', (await AsyncStorage.getItem('ledger')))
-              console.log('account>>', JSON.parse(await AsyncStorage.getItem('account')))
-            }}/>
-        </View>
-      </ViewPagerAndroid>
+            {this.state.currentScreen === currentScreen.detailAccount &&
+            <AccountDetail childData={this.state.childData}/>}
+          </View>
+          <View key="3">
+            <Text>Dev Page</Text>
+            <Button
+              title="Remove All"
+              onPress={async () => {
+                await AsyncStorage.clear()
+                console.log('removed all')
+              }}/>
+            <View style={{height: 20}}/>
+            <Button
+              title="Seed All Data"
+              onPress={async () => {
+                await AsyncStorage.setItem('ledger', JSON.stringify([
+                  {lI: "1", lN: "L-4", lA: 400, lC: "1", lNo: "Testing 4", lD: 1584884861245, aI: "3"},
+                  {lI: "2", lN: "L-3", lA: 540, lC: "2", lNo: "Testing 3", lD: 1584884703808, aI: "1"},
+                  {lI: "3", lN: "L-2", lA: 200, lC: "3", lNo: "Testing 2", lD: 1584884528782, aI: "3"},
+                  {lI: "4", lN: "L-1", lA: 100, lC: "1", lNo: "Testing 1", lD: 1584884455243, aI: "2"}
+                ]))
+                await AsyncStorage.setItem('account', JSON.stringify([
+                  {aI: '1', aN: 'A-1', aA: 20000, d: false, lg: [{add: 5000}]},
+                  {aI: '2', aN: 'A-2', aA: 40000, d: true, lg: [{add: 10000}]},
+                  {aI: '3', aN: 'A-3', aA: 60000, d: false, lg: [{add: 30000}]}
+                ]))
+                await AsyncStorage.setItem('category', JSON.stringify([
+                  {cI: '1', cN: 'Other'},
+                  {cI: '2', cN: 'C-2'},
+                  {cI: '3', cN: 'C-3'}
+                ]))
+                console.log("seeding done")
+              }}/>
+            <View style={{height: 20}}/>
+            <Button
+              title="Show Data"
+              onPress={async () => {
+                console.log('ledger>>', (await AsyncStorage.getItem('ledger')))
+                console.log('account>>', JSON.parse(await AsyncStorage.getItem('account')))
+              }}/>
+          </View>
+        </ViewPagerAndroid>
+        <View style={styles.footer}><Text>TEST</Text></View>
+      </>
     )
   }
 }
@@ -138,5 +141,12 @@ const styles = StyleSheet.create({
     padding: 15,
     textAlign: 'left',
     backgroundColor: '#4a6c8c'
+  },
+  footer: {
+    color: '#4a6c8c',
+    fontSize: 24,
+    padding: 15,
+    textAlign: 'left',
+    backgroundColor: '#c9dbec'
   }
 })
