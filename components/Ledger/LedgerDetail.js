521a64158de8515754aa23ef180df3ed04f06b6c
sumil <sumilprincejain@gmail.com>
2020-05-07 01:35:12 +0530

    updation in ledger detail

--- components/Ledger/LedgerDetail.js
+++ components/Ledger/LedgerDetail.js
@@ -3,6 +3,7 @@ import { Text, View, TextInput, FlatList, Picker, Alert, ToastAndroid } from "re
 import FabButton from '../FabButton'
 import { Color, style } from '../../Constants'
 import { insertLedger, fetchAllAccounts, fetchAllCategories } from '../../db'
+import { isValid } from "./common"
 
 let initialState = {
   ledgerName: "",
@@ -17,40 +18,28 @@ class LedgerDetail extends Component {
   state = {
     category: [],
     accounts: [],
-    showCalender: false
+    showCalender: false,
+    editMode: false
   }
 
   async componentDidMount() {
-    const {result : accounts} = await fetchAllAccounts()
-    let accountDefault = accounts.filter(a=>a.defaultAccount);
-    accountDefault = accountDefault && accountDefault[0] ? accountDefault[0] : accounts[0];
-    const {result : category} = await fetchAllCategories()
+    const {result: accounts} = await fetchAllAccounts()
+    let accountDefault = accounts.filter(a => a.defaultAccount)
+    accountDefault = accountDefault && accountDefault[0] ? accountDefault[0] : accounts[0]
+    const {result: category} = await fetchAllCategories()
 
-    if(this.props.childData && this.props.childData.ledger){
-      this.setState({ accounts, category, ...this.props.childData.ledger});
-    }
-    else
-      this.setState({...initialState, accounts, category, accountId:accountDefault.accountId})
+    if (this.props.childData && this.props.childData.ledger) {
+      this.setState({accounts, category, ...this.props.childData.ledger})
+    } else
+      this.setState({...initialState, accounts, category, accountId: accountDefault.accountId})
   }
 
   handleOnSave = async () => {
-    let isError = false
-    if (!validate(this.state, 'ledgerName')) {
-      ToastAndroid.show('Name is mandatory', ToastAndroid.SHORT)
-      isError = true
-    }
-    if (!validate(this.state, 'ledgerAmount')) {
-      ToastAndroid.show('Amount is mandatory', ToastAndroid.LONG)
-      isError = true
-    }
-    if (!validate(this.state, 'ledgerCategory')) {
-      ToastAndroid.show('Category is mandatory', ToastAndroid.LONG)
-      isError = true
-    }
-    if (!isError) {
+    if (isValid({...this.state})) {
       const res = await insertLedger(this.state)
       if (res.success) {
-        this.setState({...initialState})
+        ToastAndroid.show('Expense Updated', ToastAndroid.SHORT)
+        this.setState({...initialState, editMode: false})
       }
     }
   }
@@ -65,6 +54,7 @@ class LedgerDetail extends Component {
           onChangeText={ledgerName => this.setState({ledgerName})}
           placeholder={'Expense'}
           value={this.state.ledgerName}
+          editable={this.state.editMode}
         />
         <View style={{height: 20}}/>
         <TextInput
@@ -73,62 +63,70 @@ class LedgerDetail extends Component {
           placeholder={'0.00'}
           keyboardType={'decimal-pad'}
           value={this.state.ledgerAmount + ''}
+          editable={this.state.editMode}
         />
         <View style={{height: 20}}/>
-        <Picker
-          selectedValue={this.state.ledgerCategory}
-          style={{height: 50}}
-          onValueChange={ledgerCategory => this.setState({ledgerCategory})}>
-          {
-            this.state.category.map(a => <Picker.Item label={a.categoryName} value={a.categoryId} key={a.categoryId}/>)
-          }
-        </Picker>
-
         <TextInput
           value={this.state.ledgerNotes + ''}
-          style={{borderWidth: 1}}
+          style={style.inputText}
+          placeholder={'Description (if any)'}
           multiline
-          numberOfLines={16}
+          numberOfLines={1}
           onChangeText={ledgerNotes => this.setState({ledgerNotes})}
-          editable
+          editable={this.state.editMode}
           maxLength={200}
         />
+        <View style={{height: 20}}/>
 
-        <Picker
-          selectedValue={this.state.accountId}
-          style={{height: 50}}
-          onValueChange={accountId => this.setState({accountId})}>
-          {
-            this.state.accounts.map(a => <Picker.Item label={a.accountName} value={a.accountId} key={a.accountId}/>)
-          }
-        </Picker>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Text style={{flex: 1, textAlignVertical: 'center'}}>Category</Text>
+          <Picker
+            enabled={this.state.editMode}
+            style={{height: 40, flex: 2}}
+            selectedValue={this.state.ledgerCategory}
+            onValueChange={ledgerCategory => this.setState({ledgerCategory})}>
+            {
+              this.state.category.map(a => <Picker.Item label={a.categoryName} value={a.categoryId}
+                                                        key={a.categoryId}/>)
+            }
+          </Picker>
+        </View>
+
+        <View style={{height: 10}}/>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Text style={{flex: 1, textAlignVertical: 'center'}}>Account</Text>
+          <Picker
+            enabled={this.state.editMode}
+            selectedValue={this.state.accountId}
+            style={{height: 40, flex: 2}}
+            onValueChange={accountId => this.setState({accountId})}>
+            {
+              this.state.accounts.map(a => <Picker.Item label={a.accountName} value={a.accountId} key={a.accountId}/>)
+            }
+          </Picker>
+        </View>
+        <View style={{height: 20}}/>
 
         <View>
           <Text>Date</Text>
         </View>
 
-        <FabButton
+        <View style={{height: 20}}/>
+
+        {!this.state.editMode && <FabButton
           text="&#9998;"
-          textStyle={{fontSize:30}}
-          style={{}}
+          textStyle={{fontSize: 30}}
+          onPress={() => this.setState({editMode: true})}
+        />}
+
+        {this.state.editMode && <FabButton
+          text="&#10003;"
+          textStyle={{fontSize: 30}}
           onPress={() => this.handleOnSave()}
-        />
+        />}
       </View>
     )
   }
 }
 
 export default LedgerDetail
-
-
-const validate = (obj, key) => {
-  if (obj.hasOwnProperty(key)) {
-    const value = obj[key]
-    if (typeof value === 'string' && !value.length) {
-      return false
-    } else if (typeof value === 'number' && !(value > 0))
-      return false
-    return true
-  }
-  return false
-}
